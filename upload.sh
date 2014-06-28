ce1060a29f81f858c7db8c417d9c4f3d1cb28c40
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2014-06-28 17:08:29 +0200

    added a upload script

---
+++ upload.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Usage:
+# upload.sh USER
+
+cp -R . ~/tmp
+cd ~/tmp
+rm -rf .git
+rm .gitattributes
+rm .gitignore
+rm LICENSE
+rm README.md
+rm Readme.md
+rm upload.sh
+
+scp -r . $1,cppcheck@web.sf.net:htdocs/
+
+cd ~
+rm -rf tmp
