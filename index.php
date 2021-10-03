9e283cbf3bd48f620862e8f0e88776cec7b69380
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2021-10-03 09:52:29 +0200

    log visitor ip

--- index.php
+++ index.php
@@ -289,5 +289,40 @@ This has been described in a few articles (<a href="https://www.theregister.co.u
 </div> <!-- .wrap -->
 </div> <!-- #content -->
 <?php include_once("analyticstracking.php") ?>
+<?php
+function getUserIP()
+{
+    // Get real visitor IP behind CloudFlare network
+    if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+              $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+              $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+    }
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    return $ip;
+}
+
+$t = date("Y-m-d h:i:s");
+$ip = getUserIP();
+
+$file = fopen("ip.txt","a");
+fwrite($file,"$t $ip\n");
+fclose($file);
+?>
 </body>
 </html>
