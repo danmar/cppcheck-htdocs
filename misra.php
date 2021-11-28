65d0616f080079303f4425d28e7314e0926cb110
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2021-11-28 20:56:15 +0100

    update misra feature matrix

--- misra.php
+++ misra.php
@@ -33,11 +33,7 @@
 <div id="content">
   <div class="wrap">
 <h1>Cppcheck - MISRA C 2012 Compliance</h1>
-<p>The MISRA C 2012 compliance checking in cppcheck is a work in progress.</p>
-
-<h2>Kickstarter</h2>
-
-<p>We are currently trying to get funding for misra development through <a href="https://www.kickstarter.com/projects/3300446/improve-misra-checking">kickstarter</a>. Please donate to make the misra addon more complete.</p>
+<p>Cppcheck covers almost all the MISRA C 2012 rules. Including the amendments. Together with a C compiler you get full coverage.</p>
 
 <h2>Feature matrix</h2>
 
@@ -45,17 +41,15 @@
 
 <p>The rules 1.1 and 1.2 will not be handled by Cppcheck. If you want to enforce these you can use compilers. For instance, you can compile (but not necessarily link) with gcc + msvc + your normal target compiler.</p>
 
-<p>There are several other missing MISRA checks in the table below. Cppcheck will have these checks too, we will work on that, but if you want to check compliance now you can use a compiler as a complement (for instance gcc cover some of the missing rules).</p>
-
 <pre>
 1.1     
 1.2     
 1.3     X (Cppcheck)
 2.1     X (Cppcheck)
-2.2     X (Cppcheck)
-2.3     
-2.4     X (Cppcheck)
-2.5     
+2.2     X (Addon)
+2.3     X (Addon)
+2.4     X (Addon)
+2.5     X (Addon)
 2.6     X (Cppcheck)
 2.7     X (Addon)
 3.1     X (Addon)
@@ -67,45 +61,45 @@
 5.3     X (Cppcheck)
 5.4     X (Addon)
 5.5     X (Addon)
-5.6     
-5.7     
-5.8     
-5.9     
+5.6     X (Addon)
+5.7     X (Addon)
+5.8     X (Addon)
+5.9     X (Addon)
 6.1     X (Addon)
 6.2     X (Addon)
 7.1     X (Addon)
 7.2     X (Addon)
 7.3     X (Addon)
 7.4     X (Addon)
-8.1     
-8.2     
+8.1     X (Addon)
+8.2     X (Addon)
 8.3     X (Cppcheck)
-8.4     
-8.5     
-8.6     
-8.7     
-8.8     
-8.9     
-8.10    
+8.4     X (Addon)
+8.5     X (Addon)
+8.6     X (Addon)
+8.7     X (Addon)
+8.8     X (Addon)
+8.9     X (Addon)
+8.10    X (Addon)
 8.11    X (Addon)
 8.12    X (Addon)
-8.13    
+8.13    X (Cppcheck)
 8.14    X (Addon)
-9.1     
+9.1     X (Cppcheck)
 9.2     X (Addon)
-9.3     
-9.4     
+9.3     X (Addon)
+9.4     X (Addon)
 9.5     X (Addon)
 10.1    X (Addon)
 10.2    X (Addon)
-10.3    
+10.3    X (Addon)
 10.4    X (Addon)
-10.5    
+10.5    X (Addon)
 10.6    X (Addon)
-10.7    
+10.7    X (Addon)
 10.8    X (Addon)
-11.1    
-11.2    
+11.1    X (Addon)
+11.2    X (Addon)
 11.3    X (Addon)
 11.4    X (Addon)
 11.5    X (Addon)
@@ -121,7 +115,7 @@
 13.2    X (Cppcheck)
 13.3    X (Addon)
 13.4    X (Addon)
-13.5
+13.5    X (Addon)
 13.6    X (Addon)
 14.1    X (Addon)
 14.2    X (Addon)
@@ -134,7 +128,7 @@
 15.5    X (Addon)
 15.6    X (Addon)
 15.7    X (Addon)
-16.1    
+16.1    X (Addon)
 16.2    X (Addon)
 16.3    X (Addon)
 16.4    X (Addon)
@@ -144,7 +138,7 @@
 17.1    X (Addon)
 17.2    X (Addon)
 17.3    
-17.4    
+17.4    X (Cppcheck)
 17.5    X (Cppcheck)
 17.6    X (Addon)
 17.7    X (Addon)
@@ -157,7 +151,7 @@
 18.6    X (Cppcheck)
 18.7    X (Addon)
 18.8    X (Addon)
-19.1    
+19.1    X (Cppcheck)
 19.2    X (Addon)
 20.1    X (Addon)
 20.2    X (Addon)
@@ -166,15 +160,15 @@
 20.5    X (Addon)
 20.6    X (Cppcheck)
 20.7    X (Addon)
-20.8    
-20.9    
+20.8    X (Addon)
+20.9    X (Addon)
 20.10   X (Addon)
-20.11   
-20.12   
+20.11   X (Addon)
+20.12   X (Addon)
 20.13   X (Addon)
 20.14   X (Addon)
 21.1    X (Addon)
-21.2    
+21.2    X (Addon)
 21.3    X (Addon)
 21.4    X (Addon)
 21.5    X (Addon)
@@ -185,12 +179,25 @@
 21.10   X (Addon)
 21.11   X (Addon)
 21.12   X (Addon)
+21.13   X (Cppcheck)
+21.14   X (Addon)
+21.15   X (Addon)
+21.16   X (Addon)
+21.17   X (Cppcheck)
+21.18   X (Cppcheck)
+21.19   X (Addon)
+21.20   X (Addon)
+21.21   X (Addon)
 22.1    X (Cppcheck)
 22.2    X (Cppcheck)
-22.3    
+22.3    X (Cppcheck)
 22.4    X (Cppcheck)
-22.5    
+22.5    X (Addon)
 22.6    X (Cppcheck)
+22.7    X (Addon)
+22.8    X (Addon)
+22.9    X (Addon)
+22.10   X (Addon)
 </pre>
 </div>
 </div>
