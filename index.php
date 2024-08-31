3eb31e7157dbe9723df67091d8aee51fd7b1a8f5
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2024-08-31 18:17:04 +0200

    2.15.0: download links

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ Cppcheck is designed to be able to analyze your C/C++ code even if it has non-st
 
 <h2 id="download">Download</h2>
 
-<h3>Cppcheck 2.14 (open source)</h3>
+<h3>Cppcheck 2.15 (open source)</h3>
 <table width="80%" class="table" >
   <tr>
     <th width="75%">Platform</th>
@@ -68,15 +68,15 @@ Cppcheck is designed to be able to analyze your C/C++ code even if it has non-st
   </tr>
   <tr>
     <td>Windows 64-bit (No XP support)</td>
-    <td><a href="https://github.com/danmar/cppcheck/releases/download/2.14.1/cppcheck-2.14.1-x64-Setup.msi">Installer</a></td>
+    <td><a href="https://github.com/danmar/cppcheck/releases/download/2.15.0/cppcheck-2.15.0-x64-Setup.msi">Installer</a></td>
   </tr>
   <tr>
     <td>Source code (.zip)</td>
-    <td><a href="https://github.com/danmar/cppcheck/archive/2.14.1.zip">Archive</a></td>
+    <td><a href="https://github.com/danmar/cppcheck/archive/2.15.0.zip">Archive</a></td>
   </tr>
   <tr>
     <td>Source code (.tar.gz)</td>
-    <td><a href="https://github.com/danmar/cppcheck/archive/2.14.1.tar.gz">Archive</a></td>
+    <td><a href="https://github.com/danmar/cppcheck/archive/2.15.0.tar.gz">Archive</a></td>
   </tr>
 </table>
 
